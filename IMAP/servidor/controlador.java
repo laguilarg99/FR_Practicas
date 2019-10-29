bdc40f958c3f597b052b2a0220bb008eee5677d9
Iqqdd99 <pacoaterribas@gmail.com>
2019-10-29 18:42:51 +0100

    Argumentos se pueden leer

--- IMAP/servidor/controlador.java
+++ IMAP/servidor/controlador.java
@@ -12,24 +12,49 @@ public class controlador implements Runnable
 {
 	private Socket socketServicio;
 	
-	public controlador(Socket socketServicio) {
+    public controlador(Socket socketServicio) 
+    {
 		this.socketServicio=socketServicio;
 	}
 	
-	public void run(){		
+    public void run()
+    {		
 		try {
-			
 			PrintWriter outPrintWriter = new PrintWriter(socketServicio.getOutputStream(),true);
 			BufferedReader inReader = new BufferedReader(new InputStreamReader(socketServicio.getInputStream()));
 
-            String peticion = new String(inReader.readLine());
-			String respuesta=yodaDo(peticion);
-			outPrintWriter.print(respuesta + "\r\n" );
-			outPrintWriter.flush();
-			
-		} catch (IOException e) {
-			System.err.println("Error al obtener los flujso de entrada/salida.");
-		}
+            if((peticion = inReader.readLine()) != null){
+                String[] Argumento = obtenerArgumentos(peticion);
 
-	}
+                switch(Argumento[0]) {
+                    case "AUTH":
+                      // code block
+                      break;
+                    case "MEntra":
+                      // code block
+                      break;
+                    default:
+                      // code block
+                  }
+            } else {
+			    System.err.println("No se han recibido datos.");
+            }
+            
+            /*
+            outPrintWriter.print(respuesta + "\r\n" );
+			outPrintWriter.flush();
+			*/
+        } catch (IOException e)
+            {
+			    System.err.println("Error al obtener los flujo de entrada/salida.");
+		    }
+
+    }
+    
+    private String[] obtenerArgumentos(String Cadena)
+    {
+        Cadena = (Cadena.substring(0,Cadena.length()-1)).substring(1);
+        String[] args = Cadena.split("#");
+        return args; 
+    }
 }
